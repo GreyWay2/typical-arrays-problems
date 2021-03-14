dad7f0b07645acb5edc1f5ae8f1ccd310f32bbe7
GreyWay2 <greenpeesoleg@gmail.com>
2021-03-14 11:14:05 +0400

    feat: create functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array=[0]) {
+  if (array.length==0) {
+    return 0;
+  }   
+  var min=array[0];
+  for(var i=0;i<array.length;i++){
+    if(array[i]<min) {
+      min=array[i];
+    }
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array=[0]) {
+  if (array.length==0) {
+    return 0;
+  }   
+  var max=array[0];
+  for(var i=0;i<array.length;i++){
+    if(array[i]>max) {
+      max=array[i];
+    }
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array=[0]) {
+  if (array.length==0) {
+    return 0;
+  }   
+  var summ = 0;
+  for(var i=0;i<array.length;i++){
+    summ+=array[i]
+    }
+  return summ/array.length;
 }
